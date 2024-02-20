eef93283db59683d7eaf30ca76ff31ade9885805
DmitriX <dodge.241294@gmail.com>
2024-02-20 12:43:19 +0300

    add: class DataGetter was added
    *added private  method getDataFromServer
    *added private method normalizeData
    *added public method getData

---
+++ src/js/DataGetter.js
@@ -0,0 +1,40 @@
+export class DataGetter {
+    static async #getDataFromServer(apiUrl) {
+        const response = await fetch(apiUrl);
+        if (response.ok) {
+            return await response.json();
+        } else {
+            return false;
+        }
+    }
+
+    static #normalizeData(data) {
+        function normalize(parentService) {
+            data.services.forEach((service) => {
+                if (service.head === parentService.id) {
+                    if (!parentService?.services) parentService.services = [];
+                    parentService.services.push(service);
+                    normalize(service);
+                }
+                else {
+                    if (parentService.services) parentService.services.sort((service1, service2) => service1.sorthead - service2.sorthead)
+                    return;
+                }
+            });
+        }
+        const normalizedData = {
+            id: null,
+            services: [],
+        };
+
+        normalize(normalizedData)
+
+        return normalizedData;
+    }
+
+    static async getData(apiUrl) {
+        const data = await DataGetter.#getDataFromServer(apiUrl);
+        const normalizedData = DataGetter.#normalizeData(data)
+        return normalizedData;
+    }
+}
