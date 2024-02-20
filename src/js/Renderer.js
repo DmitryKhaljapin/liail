1867c75967f519c5863ae66f29d35a3056ed5ced
DmitriX <dodge.241294@gmail.com>
2024-02-20 12:45:53 +0300

    add: class Renderer was added
    *added private method createHTMLElement
    *added private method render
    *added public method renderTree

---
+++ src/js/Renderer.js
@@ -0,0 +1,37 @@
+export class Renderer {
+    static #createHTMLElement(tag, children) {
+        const HTMLElement = document.createElement(tag);
+
+        if (children.length) {
+            children.forEach(child => HTMLElement.append(child));
+        }
+
+        return HTMLElement
+    }
+
+    static #render(HTMLElement, rootId) {
+        const rootElement = document.getElementById(rootId);
+
+        rootElement.append(HTMLElement);
+    }
+
+    static renderTree(data) {
+        function createHTMLOList(elements) {
+            return Renderer.#createHTMLElement('ol', elements.map(element => {
+                if (element.node)  {
+                    return Renderer.#createHTMLElement('li', [`${element.name}`, createHTMLOList(element.services)]);
+                }
+                return Renderer.#createHTMLElement('li', [`${element.name} ${element.node? '': `(${element.price})`}`]);
+            }));
+        }
+
+        const list = createHTMLOList(data.services);
+
+        Renderer.#render(list, 'app');
+    }
+
+    static test() {
+        const p = Renderer.#createHTMLElement('p', [Renderer.#createHTMLElement('span', ['this is span with text'])])
+        Renderer.#render(p, 'app');
+    }
+}
